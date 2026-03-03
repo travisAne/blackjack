0e6dca21507d587769e33d355994fde1fa4befc9
travisAne <travispadera@gmail.com>
2026-03-03 05:56:50 +0200

    if else

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard + 4
 
-let sum = firstCard + secondCard
 
-console.log(sum)
+
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
