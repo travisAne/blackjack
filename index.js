1d545a14323a7fcd09ac8434faf1d67acee022e5
travisAne <travispadera@gmail.com>
2026-03-15 08:29:49 +0200

    designing

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 5
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
 
+let sumEl = document.querySelector("body")
 
-
-
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    console.log("You're out of the game! 😭")
+function startGame() {
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-
