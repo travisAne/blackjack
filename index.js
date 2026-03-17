49c19fa6a7f7366f06b3c4955e663b0b2b2baf3b
travisAne <travispadera@gmail.com>
2026-03-17 05:39:15 +0200

    new button

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let cardsEl = document.getElementById("cardsEl")
 
 let sumEl = document.querySelector("body")
 
 function startGame() {
     
     sumEl.textContent = "Sum: " + sum
+    sumEl.textContent = "Cards: 10 4"
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
-        hasBlackJack = true
+        hasBlackJack = true 
     } else {
         message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 6
+    sum += card
+    startGame()
 }
