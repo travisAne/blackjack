4e230cd1f996acdac0b5f8ccdeb04e9a6571d53f
travisAne <travispadera@gmail.com>
2026-04-03 10:04:57 +0200

    Blackjack complete

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let player = {
+    name: "Per",
+    chips: 200
+}
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,18 +11,19 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    return Math.floor( Math.random()*13 ) + 1
-    if (randomNumer > 10) {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
         return 10
-    } else if (randomNumer === 1) {
+    } else if (randomNumber === 1) { 
         return 11
     } else {
-        return randomNumer
+        return randomNumber
     }
-
 }
 
 function startGame() {
@@ -26,12 +32,6 @@ function startGame() {
     let secondCard = getRandomCard()
     cards = [firstCard, secondCard]
     sum = firstCard + secondCard
-    
-    renderGame()
-}
-
-
-function startGame() {
     renderGame()
 }
 
@@ -56,10 +56,10 @@ function renderGame() {
 
 
 function newCard() {
-    
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
